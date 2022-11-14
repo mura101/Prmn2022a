8cdeee85122b99cde101c209b6ab4704d7f539b3
mura101 <b2202370@photon.chitose.ac.jp>
2022-11-14 16:26:34 +0900

    3-1終了

---
+++ prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,13 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("任意の文字を入力してください。");
+        String s = scanner.next();
+        System.out.println("\""+s+"\"と入力されました。");
+
+    }
+}
